0ad904a93e4856efe5d8f0295003501165a2d0ba
Lee Wisener <wisenerl@Lees-Mac-Studio.local>
2025-10-19 17:22:50 +0100

    Deprivation Index

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,7 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
-return Application::configure(basePath: dirname(__DIR__))
+$app = Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
@@ -16,10 +16,11 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withExceptions(function (Exceptions $exceptions): void {
         //
     })
-    ->when(env('APP_ENV') === 'local', function (Application $app) {
-        // Register dev-only providers like Pail
-        if (class_exists(\Laravel\Pail\PailServiceProvider::class)) {
-            $app->register(\Laravel\Pail\PailServiceProvider::class);
-        }
-    })
     ->create();
+
+// Register dev-only providers like Pail only in local
+if (env('APP_ENV') === 'local' && class_exists(\Laravel\Pail\PailServiceProvider::class)) {
+    $app->register(\Laravel\Pail\PailServiceProvider::class);
+}
+
+return $app;
