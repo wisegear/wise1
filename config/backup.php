9b92fd3a339f133051d4becfc42817c5108fcfb7
Lee Wisener <wisenerl@Lees-Mac-Studio.local>
2025-11-02 14:55:07 +0000

    Backup Tweaks

--- config/backup.php
+++ config/backup.php
@@ -300,7 +300,7 @@ return [
             /*
              * The number of days for which backups must be kept.
              */
-            'keep_all_backups_for_days' => 3,
+            'keep_all_backups_for_days' => 0,
 
             /*
              * After the "keep_all_backups_for_days" period is over, the most recent backup
@@ -314,19 +314,19 @@ return [
              * of that week will be kept. Older backups within the same week will be removed.
              * If you create backups only once a week, no backups will be removed yet.
              */
-            'keep_weekly_backups_for_weeks' => 2,
+            'keep_weekly_backups_for_weeks' => 0,
 
             /*
              * After the "keep_weekly_backups_for_weeks" period is over, the most recent backup
              * of that month will be kept. Older backups within the same month will be removed.
              */
-            'keep_monthly_backups_for_months' => 6,
+            'keep_monthly_backups_for_months' => 0,
 
             /*
              * After the "keep_monthly_backups_for_months" period is over, the most recent backup
              * of that year will be kept. Older backups within the same year will be removed.
              */
-            'keep_yearly_backups_for_years' => 1,
+            'keep_yearly_backups_for_years' => 0,
 
             /*
              * After cleaning up the backups remove the oldest backup until
