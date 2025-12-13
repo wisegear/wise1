c254044d6cc1d0c1c99f47c3af632cd774a5fbc0
Lee Wisener <wisenerl@Lees-Mac-Studio.local>
2025-12-13 09:45:40 +0000

    Inflation case fix

--- app/Http/Controllers/InflationController.php
+++ app/Http/Controllers/InflationController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\InflationCpihMonthly;
+use App\Models\InflationCPIHMonthly;
 
 class InflationController extends Controller
 {
