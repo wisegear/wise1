1d0bdfe6ddd86dd2189785c3b7ec3b0ade02409b
Lee Wisener <wisenerl@Lees-Mac-mini.local>
2025-12-30 21:00:20 +0000

    Update Repossessions

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -70,7 +70,7 @@
                           d="M7.5 3.75H6A2.25 2.25 0 0 0 3.75 6v1.5M16.5 3.75H18A2.25 2.25 0 0 1 20.25 6v1.5m0 9V18A2.25 2.25 0 0 1 18 20.25h-1.5m-9 0H6A2.25 2.25 0 0 1 3.75 18v-1.5M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
                 </svg>
             </div>
-            <p class="mt-2 text-sm text-zinc-700">Compare counties, switch between quarterly &amp; yearly, break down by reason or stage.</p>
+            <p class="mt-2 text-sm text-zinc-700">An overview of Claims made across England & Wales, see who is making claims and all of the stages.  Search individual local authorities.</p>
             <div class="mt-4 inline-flex items-center text-sm font-medium text-lime-700 group-hover:underline">Open Repossessions
             </div>
         </a>
